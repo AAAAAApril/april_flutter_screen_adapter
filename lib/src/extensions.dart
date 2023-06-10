69de398350b6a46dbaac9552ac2df4f405cd94b8
April丶 <569551020@qq.com>
2023-06-11 00:45:56 +0800

    displayFeatures转换代码待验证

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -19,44 +19,21 @@ extension BuildContextExt on BuildContext {
       viewPadding: EdgeInsets.fromViewPadding(view.viewPadding, devicePixelRatio),
       viewInsets: EdgeInsets.fromViewPadding(view.viewInsets, devicePixelRatio),
       systemGestureInsets: EdgeInsets.fromViewPadding(view.systemGestureInsets, devicePixelRatio),
-      // displayFeatures: _decodeDisplayFeatures(
-      //   displayFeature: view.displayFeatures,
-      //   bounds: displayFeaturesBounds,
-      //   type: displayFeaturesType,
-      //   state: displayFeaturesState,
-      //   devicePixelRatio: devicePixelRatio,
-      //   realDevicePixelRatio: view.devicePixelRatio,
-      // ),
+      // TODO 需要验证对不对
+      displayFeatures: view.displayFeatures
+          .map<DisplayFeature>(
+            (e) => DisplayFeature(
+              bounds: Rect.fromLTRB(
+                view.devicePixelRatio * e.bounds.left / devicePixelRatio,
+                view.devicePixelRatio * e.bounds.top / devicePixelRatio,
+                view.devicePixelRatio * e.bounds.right / devicePixelRatio,
+                view.devicePixelRatio * e.bounds.bottom / devicePixelRatio,
+              ),
+              type: e.type,
+              state: e.state,
+            ),
+          )
+          .toList(growable: false),
     );
   }
 }
-
-// /// TODO 把用真实 [devicePixelRatio] 计算过的 [DisplayFeature] 转换为目标 [devicePixelRatio] 计算后的值
-// List<DisplayFeature> _decodeDisplayFeatures({
-//   required List<DisplayFeature> displayFeature,
-//   required List<double> bounds,
-//   required List<int> type,
-//   required List<int> state,
-//   required double devicePixelRatio,
-//   required double realDevicePixelRatio,
-// }) {
-//   assert(bounds.length / 4 == type.length, 'Bounds are rectangles, requiring 4 measurements each');
-//   assert(type.length == state.length);
-//   final List<DisplayFeature> result = <DisplayFeature>[];
-//   for (int i = 0; i < type.length; i++) {
-//     final int rectOffset = i * 4;
-//     result.add(DisplayFeature(
-//       bounds: Rect.fromLTRB(
-//         bounds[rectOffset] / devicePixelRatio,
-//         bounds[rectOffset + 1] / devicePixelRatio,
-//         bounds[rectOffset + 2] / devicePixelRatio,
-//         bounds[rectOffset + 3] / devicePixelRatio,
-//       ),
-//       type: DisplayFeatureType.values[type[i]],
-//       state: state[i] < DisplayFeatureState.values.length
-//           ? DisplayFeatureState.values[state[i]]
-//           : DisplayFeatureState.unknown,
-//     ));
-//   }
-//   return result;
-// }
